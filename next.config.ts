05a2c86991a076fcccd718a412b9fc7a16e465b1
devbyrieza <devbyrieza@users.noreply.github.com>
2026-07-29 01:55:31 +0700

    fix: remove unsupported eslint config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: any = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
   generateBuildId: async () => 'build-' + Date.now(),
   // output: "standalone",
 
